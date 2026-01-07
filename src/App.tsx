9e07bb2230bc57ee280bde14db074e8fb97e8f09
Krish3017 <krishramanandi2007@gmail.com>
2026-01-08 00:15:19 +0530

    SignUp/SignIn Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Dataset from "./Pages/DataSet";
+import SignIn from "./components/layouts/SignIn";
+import Signup from "./components/layouts/SignUp";
 
 function App() {
   return (
@@ -8,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<HomePage />} />
-          <Route index path='/DataSet' element={<Dataset />}/>
+          <Route index path='/DataSet' element={<Dataset />} />
+          <Route index path='/SignIn' element={<SignIn />} />
+          <Route index path='/SignUp' element={<Signup/>} />
         </Route>
       </Routes>
     </BrowserRouter>
