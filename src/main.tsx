526f59cd4d292beb440b5c17e092b731360cca87
Krish3017 <krishramanandi2007@gmail.com>
2026-01-13 01:02:42 +0530

    Minimal main.tsx fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastProvider } from "@/components/ui/toast/toast.tsx"
+import { ToastProvider } from "@/components/ui/toast/Toast.tsx"
 import "./index.css";
 import App from "./App.tsx";
 
